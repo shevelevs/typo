2f723ee218b4cc71c7893389db7f4516753cf8bb
ssh <ssh.spam@gmail.com>
2013-08-25 08:14:31 -0700

    fixed: app crashed when deployed to aws/mysql

--- spec/models/content_spec.rb
+++ spec/models/content_spec.rb
@@ -8,6 +8,29 @@ describe Content do
     @content = Content.create
   end
 
+  describe 'dates_to_strings' do
+    before do
+      class Object_with_publication
+        attr_accessor :publication
+      end
+      @stub_object =  Object_with_publication.new
+    end
+
+    it 'returns correct strings when passed time objects' do
+      @stub_object.publication = Time.parse('2013-08-25 10:03')
+      contents = [ @stub_object ]
+      Content.send :dates_to_strings, contents
+      contents[0].publication.should be == '2013-08'
+    end
+
+    it 'returns correct strings when passed string objects' do
+      @stub_object.publication = '2013-08-25 10:03'
+      contents = [ @stub_object ]
+      Content.send :dates_to_strings, contents
+      contents[0].publication.should be == '2013-08'
+    end
+  end
+
   describe "#short_url" do
     before do
       @content.published = true
